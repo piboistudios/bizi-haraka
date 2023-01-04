ae34300d0f111aa2669ba87678ba9bf5bac4582b
topboy <topboy@pop-os.localdomain>
2023-01-04 17:08:11 -0600

    self sign

---
+++ init_tls.js
@@ -0,0 +1,17 @@
+async function main() {
+    var selfsigned = require('selfsigned');
+    const attrs = [{ name: 'commonName', value: process.env.HOSTNAME || 'just.trust.me' }];
+
+    /**@type {import('selfsigned').GenerateResult} */
+    const pems = await new Promise((resolve, reject) => selfsigned.generate(attrs, { days: 90 }, (err, pems) => {
+        if (err) return reject(err);
+        resolve(pems);
+    }));
+
+    const fs = require('fs');
+
+    Object.entries(pems).forEach(([file, contents]) => {
+        fs.writeFileSync('config/' + file + '.pem', contents);
+    });
+}
+main()
